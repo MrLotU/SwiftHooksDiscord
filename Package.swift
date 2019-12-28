db88bc1b0fefe139f5b145fa47daf8a434d8754a
MrLotU <j.koopman@jarict.nl>
2019-12-28 14:31:55 +0100

    Depend on GH version of SwiftHooks instead of local

--- Package.swift
+++ Package.swift
@@ -11,10 +11,10 @@ let package = Package(
         .library(name: "Discord", targets: ["Discord"]),
     ],
     dependencies: [
-        .package(path: "../SwiftHooks"),
+        .package(url: "https://github.com/MrLotU/SwiftHooks.git", .branch("master")),
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.11.1"),
         .package(url: "https://github.com/apple/swift-nio-zlib-support.git", from: "1.0.0"),
-        .package(path: "../../websocket-kit")
+        .package(url: "https://github.com/vapor/websocket-kit.git", .branch("WSSFixes")),
     ],
     targets: [
         .target(
